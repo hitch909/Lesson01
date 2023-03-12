93e154a83256740b5973d0f313a8c1d54c061506
Дружинин Евгений <120795996+hitch909@users.noreply.github.com>
2023-03-12 10:26:03 +0500

    Update Task02.py

--- Task02.py
+++ Task02.py
@@ -1,9 +1,24 @@
 # Задача 2: Найдите сумму цифр трехзначного числа.
 
 number = int(input("введите трехзначное число  N =   "))
-x = number % 10
-y = number // 10 % 10
-z = number // 100
-summ = x + y + z
-print(" сумма цифр из числа N = ", summ)
+#x = number % 10
+#y = number // 10 % 10
+#z = number // 100
+#summ = x + y + z
+#print(" сумма цифр из числа N = ", summ)
+
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+number = int(input("введите любое целое число  N =   "))
+
+
+def sum_of_digits(number):
+    summ = 0
+    while number > 0:
+        summ += number % 10
+        number //= 10
+    return summ
+
+
+print(sum_of_digits(number))
 
